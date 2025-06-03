9356f2bcb8def9c606848f39c0acd1e0feb42948
yuhao988 <huan0268@e.ntu.edu.sg>
2025-06-03 15:53:53 +0800

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DIY Unit FE Poke</h1>
+        <h2>Class List:</h2>
+        <Link to="/classes">Here</Link>
+        <h2>Equipment List:</h2>
+        <Link to="/equipment">Here</Link>
+        <h2>Test battles</h2>
+        <Link to="/battle">Here</Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
