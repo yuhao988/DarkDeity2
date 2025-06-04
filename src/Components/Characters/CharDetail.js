1d35bfd305a86a28f2574197eee30eade88d31c0
yuhao988 <huan0268@e.ntu.edu.sg>
2025-06-04 08:53:55 +0800

    sync data

--- src/Components/Characters/CharDetail.js
+++ src/Components/Characters/CharDetail.js
@@ -1,43 +1,47 @@
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import charActive from "../Datas/charActive.json";
+import charGrowth from "../Datas/charStatPass.json";
 import "../../App.css";
 import "./Char.css";
 
 function CharDetail() {
   const { name } = useParams(); // Gets the URL parameter (e.g., "gwyn")
-  const character = Object.values(charActive).find(
+  const character1 = Object.values(charActive).find(
+    (char) => char.Name.toLowerCase() === name
+  );
+  const character2 = Object.values(charGrowth).find(
     (char) => char.Name.toLowerCase() === name
   );
 
-  if (!character) return <div>Character not found</div>;
+  if (!character1) return <div>Character not found</div>;
 
-  const displayedRng =(rng)=>{
-    if (rng==="Attack"){
-        return "Same as attack range"
+  const displayedRng = (rng) => {
+    if (rng === "Attack") {
+      return "Same as attack range";
     } else {
-        return rng
+      return rng;
     }
-
-  }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{character.Name}</h1>
+        <h1>{character1.Name}</h1>
       </header>
       <body className="App-body">
-        <div className="character-detail">      
-      <p>Active Skill: {character.activeSkill}</p>
-      <p>Skill Effect: {character.activeEff}</p>
-      <p>Skill Type: {character.activeType}</p>
-      <p>Mana Cost: {character.activeMana}</p>
-      <p>Range: {displayedRng(character.activeRng)}</p>
-      <p>Skill Upgrade 1: {character.activeBuff1}</p>
-      <p>Skill Upgrade 2: {character.activeBuff2}</p>
-      <p>Skill Upgrade 3: {character.activeBuff3}</p>
-      <p>Skill Upgrade 4: {character.activeBuff4}</p>
-      
-    </div>
+        <div className="character-detail">
+          <p>Passive Skill: {character2.passive}</p>
+          <p>Passive Effect: {character2.passiveEff}</p>
+          <p>Active Skill: {character1.activeSkill}</p>
+          <p>Skill Effect: {character1.activeEff}</p>
+          <p>Skill Type: {character1.activeType}</p>
+          <p>Mana Cost: {character1.activeMana}</p>
+          <p>Range: {displayedRng(character1.activeRng)}</p>
+          <p>Skill Upgrade 1: {character1.activeBuff1}</p>
+          <p>Skill Upgrade 2: {character1.activeBuff2}</p>
+          <p>Skill Upgrade 3: {character1.activeBuff3}</p>
+          <p>Skill Upgrade 4: {character1.activeBuff4}</p>
+        </div>
         <Link to="/characters">Back</Link>{" "}
       </body>
     </div>
