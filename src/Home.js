b0507c9936388bc96ac642e0203861d2cad9f1a0
HuangYH988 <yuhao.huang@ntu.edu.sg>
2025-07-28 22:56:50 +0800

    updated Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 //import { Link } from "react-router-dom";
+import mainLogo from "./Components/Datas/Pictures/MainLogo.jpg";
 import React from "react";
 import "./App.css";
 
 export function paragraphBreak(text) {
-  if (typeof text !== 'string') {
-    console.warn('Expected string, got:', typeof text);
-    return text || '';
+  if (typeof text !== "string") {
+    console.warn("Expected string, got:", typeof text);
+    return text || "";
   }
 
   // Split text at <br /> tags and map to JSX elements
@@ -13,7 +14,7 @@ export function paragraphBreak(text) {
 
   return parts.map((part, index) => (
     <React.Fragment key={index}>
-      {<br/>}
+      {<br />}
       {part}
       {index < parts.length - 1 && <br />} {/* Add JSX <br> between parts*/}
     </React.Fragment>
@@ -28,11 +29,23 @@ function Home() {
       </header>
 
       <div className="page-body">
+        <img
+          src={mainLogo}
+          alt="Dark Deity 2 Logo"
+          className="main-logo" // Optional: for additional styling
+        />
         <p>
           This is a fan-made web page aiming to provide information on the game
           Dark Deity 2.
           <br /> The web page is currently under construction.
         </p>
+        <div>Parts currently unavailable:</div>
+        <ul>
+          <li>Shop items</li>
+          <li>Character Builder</li>
+          <li>Enemies</li>
+          <li>Battle Simulator</li>
+        </ul>
       </div>
     </div>
   );
