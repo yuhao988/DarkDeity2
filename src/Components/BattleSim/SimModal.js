cbf9076b79662d43ae054057f9d5ee565dff8d8a
yuhao988 <huan0268@e.ntu.edu.sg>
2025-08-15 16:59:36 +0800

    updated simmodal

--- src/Components/BattleSim/SimModal.js
+++ src/Components/BattleSim/SimModal.js
@@ -12,11 +12,58 @@ import eneDeity from "../Datas/eneDeity.json";
 import "./Sim.css";
 import "../../App.css";
 
-function effectWriteup() {
-  let test = "test1";
-  switch (test) {
+function effectWriteup(class1) {
+  switch (class1) {
     case "test1":
-      return <div>Buff example</div>;
+      return "Buff examplekkkk kkkkkkkkkkkk kkkkkkkkkkkkkk kkkkkkkkkkkk kkkkkkkkk";
+    case "Devoted":
+      return "+8 Crit from Executioner";
+    case "Nightblade":
+      return "+20 Ddg from Shadowstep";
+    case "Seeker":
+      return "+4 Vulnerable to opponent from Exposing Light";
+    case "Gallant":
+      return "+5 Dmg from Honorclad";
+    case "Ranger":
+      return "+8 Heavy Block from Shield Stance";
+    case "Cutthroat":
+      return "+1 Ddge for every point of Lck from Scrapper";
+    case "Aegis":
+      return "+20% Dmg from Reflection";
+    case "Scholar":
+      return "+2 Frt from Building Knowledge";
+    case "Elementalist":
+      return "+4 Pwr from Incoke Spirits";
+    case "Gale":
+      return "+6 Dmg from Turbine";
+    case "Conduit":
+      return "+6 Crit from Flux Overload";
+    case "Frigillan":
+      return "+5 Pwr and 25 Acc from Defensive Fighter";
+    case "Monk":
+      return "+4 Weak to opponent from Nerve Strike";
+    case "Reaper":
+      return "+100% Crit Dmg to full HP opponent from Visage of Death";
+    case "Ancarant":
+      return "+25 Ddg from Midnight Ride";
+    case "Ellisant":
+      return "+25 Blind to opponent from Blinding Light";
+    case "Champion":
+      return "-4 TSpd, +25% Pwr from Titanic";
+      case "Warden":
+      return "+6 Def from Protector";
+      case "Slayer":
+      return "20% chance for extra attack from Ricochet";
+      case "Relic Knight":
+      return "+6 Def and Frt from Unstoppable";
+      case "Dreadnought":
+      return "Cannot dodge or miss from Relentless";
+      case "Hemomancer":
+      return "+1 Pwr and Crit for every 5 missing HP from Dark Magic";
+      case "Tempest":
+      return "+1 Dmg for every excess TSpd on follow-up attacks from Swift Strike";
+      case "Ascendant":
+      return "+25 Ddg, Acc or Crit from Elemental Fist";
     default:
       return null;
   }
@@ -215,32 +262,37 @@ export default function SimModal(prop) {
             {/* Battle Display - Will update immediately on any change */}
             {(enemyActive || oppoClass) && (
               <div className="battle-container">
-                <div>
+                <div style={{ maxWidth: "20vw" }}>
                   <div className="selected-enemy">
-                    {enemyActive ? (
-                      <h3>Selected Enemy: {enemyActive.Class}</h3>
-                    ) : (
-                      <h3>Selected Opponent: {oppoClass.Class}</h3>
-                    )}
+                    <h3>
+                      Selected {enemyActive ? "Enemy" : "Opponent"}:
+                      {enemyActive ? enemyActive.Class : oppoClass.Class}
+                    </h3>
                   </div>
+
                   {enemyActive ? (
-                    <div>{battleForecast(unit, enemyActive)}</div>
+                    <div>
+                      {battleForecast(unit, enemyActive)}
+                      <p>Unit buff: {effectWriteup(unit.Class)}</p>
+                    </div>
                   ) : (
-                    <div>{battleForecast2(unit, oppoClass)}</div>
+                    <div>
+                      {battleForecast2(unit, oppoClass)}
+                      <p>Unit buff: {effectWriteup(unit.Class)}</p>
+                      <p>Opponent buff: {effectWriteup(oppoClass.Class)}</p>
+                    </div>
                   )}
-                  {effectWriteup()}
-                  {effectWriteup()}
                 </div>
                 <div className="right-content">
                   <p>
                     For 10000 simulated battles in which each side attacks first
                     for half of the time:
                   </p>
-                  {enemyActive ? (
-                    <div>{simulateBattle(unit, enemyActive)}</div>
-                  ) : (
-                    <div>{simulateBattle2(unit, oppoClass)}</div>
-                  )}
+                  <div>
+                    {enemyActive
+                      ? simulateBattle(unit, enemyActive)
+                      : simulateBattle2(unit, oppoClass)}
+                  </div>
                 </div>
               </div>
             )}
